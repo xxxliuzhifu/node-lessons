b88c0c9ba92b80ac9af5a0159591c8fcdbb37da0
zhangjun73 <zhangjun73@meituan.com>
2021-02-06 21:36:34 +0800

    fix: 函数拆解

--- lesson5/app.js
+++ lesson5/app.js
@@ -10,21 +10,53 @@ const app = new Koa()
 
 app.use(cors())
 
+// 获取url
+function getTopicUrls(text) {
+  var topicUrls = []
+  var $ = cheerio.load(text)
+  // 获取首页所有的链接
+  $('#topic_list .topic_title').each(function (idx, element) {
+    var $element = $(element)
+    var href = url.resolve(cnodeUrl, $element.attr('href'))
+    topicUrls.push(href)
+  })
+  return topicUrls.slice(0, 5)
+}
+
+// 获取信息
+function getAuthorInfo(item) {
+  const $1 = cheerio.load(item.text)
+  const title = $1('#content .topic_full_title:last')
+    .contents()
+    .filter(function () {
+      return this.nodeType === 3
+    })
+    .text()
+    .trim()
+  const firstPanel = $1('#content .panel .cell')
+    .eq(0)
+    .find('.markdown-text p')
+    .text()
+    .trim()
+  const author = $1('#content .panel .cell ').eq(0).find('.reply_author')
+  return { title, firstPanel, author }
+}
+
+// 获取积分
+async function getAuthorScore(authorUrl) {
+  const res = await superagent.get(authorUrl)
+  const $author = cheerio.load(res.text)
+  const score = $author('#content .unstyled .big').eq(0).text().trim()
+  return { score }
+}
+
 app.use(async (ctx, next) => {
   const data = await new Promise((resolve, reject) => {
     superagent.get(cnodeUrl).end(async function (err, res) {
       if (err) {
         return reject(err)
       }
-      var topicUrls = []
-      var $ = cheerio.load(res.text)
-      // 获取首页所有的链接
-      $('#topic_list .topic_title').each(function (idx, element) {
-        var $element = $(element)
-        var href = url.resolve(cnodeUrl, $element.attr('href'))
-        topicUrls.push(href)
-      })
-      topicUrls = topicUrls.slice(0, 10)
+      const topicUrls = getTopicUrls(res.text)
       const urlArray = topicUrls.map((item) => {
         return superagent.get(item)
       })
@@ -36,28 +68,10 @@ app.use(async (ctx, next) => {
       for (let i = 0; i < newArr.length; i++) {
         const res1 = await Promise.all(newArr[i])
         for (let i = 0; i < res1.length; i++) {
-          const item = res1[i]
-          const $1 = cheerio.load(item.text)
-          const title = $1('#content .topic_full_title:last')
-            .contents()
-            .filter(function () {
-              return this.nodeType === 3
-            })
-            .text()
-            .trim()
-          const firstPanel = $1('#content .panel .cell')
-            .eq(0)
-            .find('.markdown-text p')
-            .text()
-            .trim()
-          const author = $1('#content .panel .cell ')
-            .eq(0)
-            .find('.reply_author')
+          const { title, firstPanel, author } = getAuthorInfo(res1[i])
           try {
             const authorUrl = url.resolve(cnodeUrl, author.attr('href'))
-            const res = await superagent.get(authorUrl)
-            const $author = cheerio.load(res.text)
-            const score = $author('#content .unstyled .big').eq(0).text().trim()
+            const { score } = await getAuthorScore(authorUrl)
             ary.push({
               title: title,
               href: topicUrls[i],
@@ -70,7 +84,6 @@ app.use(async (ctx, next) => {
           }
         }
       }
-      console.log('end', ary)
       resolve(ary)
     })
   }).catch((err) => {
